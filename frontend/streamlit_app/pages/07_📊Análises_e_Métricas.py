eb8e57ac3663b7b1a3282618bd3a4e9d2764e380
Lucas Carvalho dos Santos <lcds.snf23@uea.edu.br>
2025-09-28 10:04:39 -0400

    chore: delete old files

--- frontend/streamlit_app/pages/07_📊Análises_e_Métricas.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide", page_icon="📊", page_title="Análises")
-
-st.title("📊 Análises e Métricas")
-st.markdown("<div class='custom-info'>📌 (em breve) gráficos e relatórios interativos</div>", unsafe_allow_html=True)
